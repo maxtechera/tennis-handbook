dd925976c98bf0c38ead46c03a2e1397e777411b
Max Techera <maxi.techerag@gmail.com>
2025-07-06 02:46:23 -0300

    fix: update Vercel ignore and configure Spanish locale in Docusaurus
    - Remove unnecessary entries from .vercelignore
    - Change default locale to Spanish and add English as an additional locale
    - Configure Spanish locale settings in Docusaurus
    
    This update aims to streamline the Vercel deployment process and enhance multilingual support.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -32,9 +32,15 @@ const config: Config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: "es",
+    locales: ["es", "en"],
     localeConfigs: {
+      es: {
+        label: "Español",
+        direction: "ltr",
+        htmlLang: "es",
+        calendar: "gregory",
+      },
       en: {
         label: "English",
         direction: "ltr",
